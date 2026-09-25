876b90824e6c196e02b640939d533f1526fac4ee
h774486588-byte <h774486588@gmail.com>
2026-09-25 22:05:20 +0300

    Fix backup screen layout braces

--- app/src/main/java/com/example/ui/screens/BackupScreen.kt
+++ app/src/main/java/com/example/ui/screens/BackupScreen.kt
@@ -256,7 +256,6 @@ fun BackupScreen(
                                 )
                             }
                         }
-                        }
                     }
                 }
             }
